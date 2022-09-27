c749dcc89c1b9c7f680a262041c654f68c69462d
kaareskokebok <109585723+kaareskokebok@users.noreply.github.com>
2022-09-27 10:21:27 +0200

    Update oppg2.py

--- test_23sep/oppg2.py
+++ test_23sep/oppg2.py
@@ -1,15 +1,22 @@
-alder = int(input("Hvor gammel er du? "))
+alder = input("Hvor gammel er du? ")
+while not alder.isnumeric():
+    print("Error. Oppgi et positivt heltall")
+    alder = input("Hvor gammel er du? ")
+alder = int(alder)
 
-if alder <= 18:
-    typekort = input("kjører du på dagen eller kvelden (skriv dag eller kveld)")
-    if typekort == "dag":
-        print("Dagskort, barn: 240kr")
-    if typekort == "kveld":
-        print("Kveldskort, barn: 165kr")
-        
-if alder > 18:
-    typekort = input("kjører du på dagen eller kvelden (skriv dag eller kveld)")
-    if typekort == "dag":
-        print("Dagskort, voksen: 380kr")
-    if typekort == "kveld":
-        print("Kveldskort, voksen: 275kr")
+kort_type = input("Ønsker du dagskort eller kveldskort? ")
+while kort_type != "dagskort" and kort_type != "kveldskort":
+    print("Error. Vennligst skriv dagskort eller kveldskort")
+    kort_type = input("Ønsker du dagskort eller kveldskort? ")
+
+if alder > 18 and kort_type == "dagskort":
+    pris = 380
+elif alder > 18 and kort_type == "kveldskort":
+    pris = 275
+elif alder <= 18 and kort_type == "dagskort":
+    pris = 240
+elif alder <= 18 and kort_type == "kveldskort":
+    pris = 165
+
+alder_tekst = "Voksen" if alder > 18 else "Barn"
+print(f"{kort_type.capitalize()}, {alder_tekst}: {pris} kr")
